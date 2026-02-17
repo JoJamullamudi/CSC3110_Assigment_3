aef2006074dceb3abc7f5b1c70b03021ff146f20
jojamullamudi <jose.jamul2005@gmail.com>
2026-02-16 21:23:41 -0600

    Initial commit, Made song class

---
+++ src/Song.java
@@ -0,0 +1,36 @@
+public class Song {
+    private String title;
+    private String artist;
+    private double length;
+
+    public Song(String title, String artist, double length) {
+        this.title = title;
+        this.artist = artist;
+        this.length = length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+    
+}
